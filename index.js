3b5538aaa301370232821f77d284471adbf08f30
hyunju <dlguswn3659@naver.com>
2020-01-19 22:58:37 +0900

    쉼표 빼먹음

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ class Request {
       case "ResultAction": {
         // let web_item = 1 //일단 가위라고 쳐 가정. 이걸 웹에서 받아올거임. 가위 : 1, 바위 : 0, 보 : -1
         // let game_result = 0 //일단 지금은 비긴거. 1은 이긴거, -1은 진거(스피커 값에서 웹 값 뺀 값이 0이면 비김, -1, 2는 이긴거. 나머지는 진거)
-        console.log("현재 문제의 값 : " result.GameResult)
+        console.log("현재 문제의 값 : " ,result.GameResult)
         // if (game_result == 0) { //비김
         //   game_result = "무승부에요."
         // }
